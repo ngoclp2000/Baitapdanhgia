7dd7aabedcfd442e4b8b9b9ae2b5196b40bbcef1
trinhngoc2709 <nindaphi@gmail.com>
2021-08-22 22:56:14 +0700

    Web06-MF914-TBNgoc: Xóa file cũ

--- src/api/base/BaseAPIConfig.js
+++
@@ -1,9 +0,0 @@
-import APIConfig from '@/api/config/APIConfig.js';
-import axios from "axios";
-
-var BaseApiConfig = axios.create({
-    baseURL: APIConfig,
-    headers: {'Content-Type': 'application/json'}
-})
-
-export default BaseApiConfig
--- src/router/index.js
+++
@@ -1,6 +0,0 @@
-
-import EmployeeList from '../views/dictionary/employee/EmployeeList.vue'
-export const routes = [
-    {path: '/BAProcess', component: EmployeeList,meta: { title: 'Quản lý nhân viên' }},
-]
-
